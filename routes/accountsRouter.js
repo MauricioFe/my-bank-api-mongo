75a5ed7aa6cbb31a89f3e2b2d8ca00c9a1d9137b
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-29 13:57:23 -0300

    criando assinatura das rotas

--- routes/accountsRouter.js
+++ routes/accountsRouter.js
@@ -2,11 +2,16 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/deposito', () => { });
-router.post('/saque', () => { });
+router.put('/deposito', () => { });
+router.put('/saque', () => { });
 router.get('/consultarSaldo', () => { });
 router.delete('/:agência/:conta', () => { });
 router.post('/trasnferencia', () => { });
+router.get('/mediaSaldo/:agencia', () => { });
+router.get('/menorSaldo/:numClientes', () => { });
+router.get('/maiorSaldo/:numClientes', () => { });
+router.put('/maiorSaldo/:numClientes', () => { });
+
 
 
 export default router;
