7c9c84a41ddf4ed390043a3903420bcfdb1ff3b6
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-29 13:48:11 -0300

    Definindo assinatura das rotas

--- routes/accountsRouter.js
+++ routes/accountsRouter.js
@@ -2,4 +2,11 @@ import express from 'express';
 
 const router = express.Router();
 
+router.post('/deposito', () => { });
+router.post('/saque', () => { });
+router.get('/consultarSaldo', () => { });
+router.delete('/:agência/:conta', () => { });
+router.post('/trasnferencia', () => { });
+
+
 export default router;
