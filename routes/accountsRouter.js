e3cd2b684bbfe70f345851609a23f985d8fe984d
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-06 14:58:28 -0300

    teste rota

--- routes/accountsRouter.js
+++ routes/accountsRouter.js
@@ -83,7 +83,9 @@ router.patch('/agenciaPrivate', async (req, res) => {
         res.status(500).send("Erro na requisição " + error);
     }
 });
-
+router.get('/bacon', (req, res) =>{
+    res.send('bacon');
+})
 
 
 export default router;
